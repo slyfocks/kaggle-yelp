f0eaddc7e770c39683f37a6f303069c66e5da360
slyfocks <imp@gliazzo.com>
2013-08-04 00:29:07 -0500

    created a csv with (writing grade level, user_id) pairs

--- review_parse.py
+++ review_parse.py
@@ -1,2 +1,12 @@
 __author__ = 'slyfocks'
 import nltk
+import json
+import csv
+from grade_level import grade_level
+
+
+def write_id_grades():
+    with open('yelp_training_set_review.json') as file:
+        outfile = csv.writer(open('grade_id_pairs.csv', 'w'))
+        for review in file:
+            outfile.writerow([json.loads(review)['user_id'], str(grade_level(json.loads(review)['text']))])
