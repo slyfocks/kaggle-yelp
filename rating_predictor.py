5f022fa0b476241093f98efcdfaac968b810d024
slyfocks <imp@gliazzo.com>
2013-08-05 03:06:41 -0500

    updated, changes to come tomorrow

--- rating_predictor.py
+++ rating_predictor.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import gender
 import review_parse
 import business_analysis as banal
+import funny_useful_cool as fuc
 #right now the code is agnostic to business data. coming shortly...
 with open('yelp_test_set_user.json') as file:
     data = [json.loads(line) for line in file]
@@ -85,21 +86,54 @@ def grade_star_lists():
     return [grades, stars]
 
 
+def test_id_reviews():
+    return {member['user_id']: member['review_count'] for member in data}
+
+
 def main():
+    mean_stars = fuc.mean_user_stars()
     name_list = names(user_ids)
     business_ratings = banal.predicted_business_rating()
     gender_ratings = gender_means_recommendation(genders(name_list))
     business_ids = [entry['business_id'] for entry in review_data]
+    business_review_dict = banal.review_counts_dict()
+    fuc_dict = fuc.predicted_rating()
+    user_review_average = gender.avg_user_reviews()
+    bus_review_average = banal.avg_review_counts()
+    avg_fuc = fuc.mean_fuc()
+    id_stars_dict = gender.id_stars()
+    fuc_count = fuc.total_fuc()
+    review_count_dict = test_id_reviews()
+    training_review_count_dict = gender.id_reviews()
     for i in range(len(review_data)):
         try:
             business_rating = business_ratings[business_ids[i]]
+            business_review_counts = business_review_dict[business_ids[i]]
+            fuc_rating = fuc_dict[user_ids[i]]
+            user_fuc = fuc_count[user_ids[i]]
+            #user_stars = id_stars_dict[user_ids[i]]
+            #user_review_count = review_count_dict[user_ids[i]]
         except KeyError:
             business_rating = gender_ratings[i]['Stars']
+            business_review_counts = 1
+            user_fuc = avg_fuc
+            fuc_rating = mean_stars
+            #user_stars = mean_stars
+            '''try:
+                user_review_count = training_review_count_dict[user_ids[i]]
+            except KeyError:
+                print('goop')
+                user_review_count = user_review_average'''
         gender_rating = gender_ratings[i]['Stars']
-        overall_rating = (business_rating + gender_rating)/2
+        '''rating_numerator = ((bus_review_average + user_fuc)*fuc_rating
+                         + (business_review_counts + avg_fuc)*business_rating
+                         + (gender_rating - mean_stars))
+        rating_denominator = (business_review_counts + avg_fuc) + (bus_review_average + user_fuc)'''
+        #overall_rating = rating_numerator/rating_denominator
+        overall_rating = business_rating*gender_rating/mean_stars
         gender_ratings[i]['Stars'] = overall_rating
     keys = ['RecommendationId', 'Stars']
-    f = open('businesspeople.csv', 'w')
+    f = open('businesspeoplemod.csv', 'w')
     dict_writer = csv.DictWriter(f, keys)
     dict_writer.writer.writerow(keys)
     dict_writer.writerows(gender_ratings)
