b3703fcb8ca5779817ac45d16ae5bd3f4bf7f1e6
slyfocks <imp@gliazzo.com>
2013-08-05 20:21:39 -0500

    lots moar functions

--- business_analysis.py
+++ business_analysis.py
@@ -4,14 +4,17 @@ import numpy as np
 import funny_useful_cool as fuc
 
 with open('yelp_training_set_business.json') as file:
-    business_data = [json.loads(line) for line in file]
+    training_business_data = [json.loads(line) for line in file]
 
 with open('yelp_test_set_business.json') as file:
     test_business_data = [json.loads(line) for line in file]
 
+with open('yelp_test_set_review.json') as file:
+    review_businesses = [json.loads(entry)['business_id'] for entry in file]
+
 
 def training_businesses():
-    return [entry['business_id'] for entry in business_data]
+    return [entry['business_id'] for entry in training_business_data]
 
 
 def test_businesses():
@@ -19,7 +22,7 @@ def test_businesses():
 
 
 def training_review_counts():
-    return [entry['review_count'] for entry in business_data]
+    return [entry['review_count'] for entry in training_business_data]
 
 
 def test_review_counts():
@@ -40,6 +43,16 @@ def avg_test_review_counts():
     return sum(test_review_counts())/len(test_review_counts())
 
 
+#displays the business_ids that are in the training set and final review set: len = 4380
+def review_training_businesses():
+    return [id for id in training_businesses() if id in review_businesses]
+
+
+#displays the business_ids that are in the training set and final review set: len = 1205
+def review_test_businesses():
+    return [id for id in test_businesses() if id in review_businesses]
+
+
 def categories(entry):
     return entry['categories']
 
@@ -51,7 +64,7 @@ def category_set(business_data):
 
 def category_rating(category_set):
     category_dict = {category: [] for category in category_set}
-    for entry in business_data:
+    for entry in training_business_data:
         stars = entry['stars']
         review_count = entry['review_count']
         for category in entry['categories']:
@@ -60,9 +73,9 @@ def category_rating(category_set):
 
 
 def average_category_rating():
-    star_rating_dict = category_rating(category_set(business_data))
+    star_rating_dict = category_rating(category_set(training_business_data))
     average_rating_dict = {}
-    for category in category_set(business_data):
+    for category in category_set(training_business_data):
         total_rating = 0
         total_reviews = 0
         for rating_pairs in star_rating_dict[category]:
@@ -76,7 +89,7 @@ def predicted_business_rating():
     id_rating_dict = {}
     mean = fuc.mean_user_stars()
     category_ratings = average_category_rating()
-    for entry in business_data:
+    for entry in training_business_data:
         actual_rating = entry['stars']
         sum_expected_rating = sum([category_ratings[category] for category in categories(entry)])
         num_categories = len(categories(entry))
@@ -92,7 +105,7 @@ def predicted_business_rating():
             predicted_rating = id_rating_dict[entry['business_id']]
         except KeyError:
             sum_expected_rating = sum([category_ratings[category] for category in categories(entry)
-                                       if category in category_set(business_data)])
+                                       if category in category_set(training_business_data)])
             num_categories = len(categories(entry))
             try:
                 expected_rating = sum_expected_rating / num_categories
