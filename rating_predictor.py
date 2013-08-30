11d27e0b94a42a2f3d4a78bb4f871611c7d4c7c2
slyfocks <imp@gliazzo.com>
2013-08-29 22:39:46 -0500

    added diff score back in, this time over 2

--- rating_predictor.py
+++ rating_predictor.py
@@ -382,8 +382,8 @@ def main():
                 diff_score = partition_dict[str(partition)][0]
             except KeyError:
                 diff_score = 0
-            rating = (1.5*user_ratings[final_data[i]['user_id']]
-                      + business_ratings[final_data[i]['business_id']])/2.5
+            rating = (diff_score + user_ratings[final_data[i]['user_id']]
+                      + business_ratings[final_data[i]['business_id']])/2
             if rating > 5:
                 rating = 5.0
             elif rating < 1:
